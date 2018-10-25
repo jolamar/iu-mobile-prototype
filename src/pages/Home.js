86ff911d9990d1463fd91043630ee915d12507c6
Josh LaMar <jolamar@iu.edu>
2018-10-25 08:43:30 -0600

    refines evening cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -152,23 +152,14 @@ export class Home extends Component {
             Looking for things to do?
           </h2>
 
-          <Card title      = { "BUS-L 201" }
-                subtitle   = { "Legal Environments of Business" }
-                details    = { "5:30 PM - 6:45 PM" }
-                subdetails = { "Hodge Hall 1000" }
-                links      = {[
-                  { title: 'Getting there', url: '#' },
-                  { title: 'Assignments',   url: '#' }
-                ]}
-          />
+          <button className="rvt-button rvt-button--plain">View all events</button>
 
-          <Card title      = { "BUS-E 125" }
-                subtitle   = { "Intro to Ethics in Business" }
-                details    = { "7:00 PM - 7:45 PM" }
-                subdetails = { "Simon Hall 010" }
+          <Card title      = { "Football: Indiana vs. Purdue" }
+                details    = { "7:00 PM" }
+                subdetails = { "Memorial Stadium" }
                 links      = {[
                   { title: 'Getting there', url: '#' },
-                  { title: 'Assignments',   url: '#' }
+                  { title: 'Details',   url: '#' }
                 ]}
           />
 
@@ -192,23 +183,29 @@ export class Home extends Component {
                 ]}
           />
 
-          <h2 className="rvt-ts-23 rvt-text-bold rvt-m-top-lg rvt-m-bottom-md">Getting to class</h2>
+          <h2 className="rvt-ts-23 rvt-text-bold rvt-m-top-lg rvt-m-bottom-md">Getting home</h2>
 
 
-          <Card title      = { "Going to Kelley School" }
+          <Card title      = { "Going to Stadium" }
                 details    = {
                   <div>
-                    <div className="rvt-m-top-sm rvt-m-bottom-xs rvt-badge rvt-badge--error">A Route</div> Stadium<br />
-                    Departs in <span className="rvt-alert--success">2 mins</span> & <span className="rvt-alert--success">7 mins</span>
-                  </div>
-                }
-                subdetails={
-                  <div>
-                    <div className="rvt-m-top-sm rvt-m-bottom-xs rvt-badge rvt-badge--warning">W Limited</div> Stadium<br />
-                    Departs in <span className="rvt-alert--success">6 mins</span> & <span className="rvt-alert--success">13 mins</span>
+                    <div className="rvt-m-top-sm rvt-m-bottom-xs rvt-badge rvt-badge--error">A Route</div> Sample Gates<br />
+                    Arrives in <span className="rvt-alert--success">1 min</span> & <span className="rvt-alert--success">9 mins</span>
                   </div>
                 }
           />
+
+
+          <h2 className="rvt-ts-23 rvt-text-bold rvt-m-top-lg rvt-m-bottom-md">Tomorrow's classes</h2>
+
+          <Card title      = { "INFO-I 222" }
+                subtitle   = { "The Information Society" }
+                details    = { "2:45 PM - 4:00 PM" }
+                links      = {[
+                  { title: 'Assignments',   url: '#' }
+                ]}
+          />
+
         </React.Fragment>
       }
 
