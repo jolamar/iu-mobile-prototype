afec6ea92afdb82cefdbf6a6435c66e878a8d583
Josh LaMar <jolamar@iu.edu>
2018-10-25 16:12:40 -0600

    campus grid

--- src/pages/Campus.js
+++ src/pages/Campus.js
@@ -6,12 +6,35 @@ export class Campus extends Component {
 
   render() {
     return <div className="rvt-m-tabs__panel rvt-p-bottom-xxl" tabIndex="0" role="tabpanel" id="tab-3" aria-labelledby="t-three" hidden>
-      <Card details    = { "Bus Status" }/>
-      <Card details    = { "Locations" }/>
-      <Card details    = { "Crimson Card" }/>
-      <Card details    = { "Find Food" }/>
-      <Card details    = { "Parking" }/>
-      <Card details    = { "Labs & Printers" }/>
+
+      <div className="rvt-grid">
+        <div className="rvt-grid__item">
+          <Card details={ "Bus Status" }/>
+        </div>
+        <div className="rvt-grid__item">
+          <Card details={ "Locations" }/>
+        </div>
+      </div>
+
+      <div className="rvt-grid">
+        <div className="rvt-grid__item">
+          <Card details={ "Crimson Card" }/>
+        </div>
+        <div className="rvt-grid__item">
+          <Card details={ "Find Food" }/>
+        </div>
+      </div>
+
+      <div className="rvt-grid">
+        <div className="rvt-grid__item">
+          <Card details={ "Parking" }/>
+        </div>
+        <div className="rvt-grid__item">
+          <Card details={ "Labs & Printers" }/>
+        </div>
+      </div>
+
+
     </div>;
   }
 }
