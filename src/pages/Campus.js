5fa1c31964187aaf1a7c75765c74a98e6cad53f6
Josh LaMar <jolamar@iu.edu>
2018-10-25 13:49:33 -0600

    stub classes and campus tabs

--- src/pages/Campus.js
+++ src/pages/Campus.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
 
-// Icons
-import { IconPlus } from "../icons";
+import { Card } from "../components";
 
 export class Campus extends Component {
 
   render() {
-    return <div className="rvt-m-tabs__panel" tabIndex="0" role="tabpanel" id="tab-2" aria-labelledby="t-two" hidden>
-      <button className="rvt-m-card rvt-m-card--empty">
-        <div className="rvt-m-card__content">
-          { IconPlus }
-        </div>
-      </button>
-      <button className="rvt-m-card rvt-m-card--empty">
-        <div className="rvt-m-card__content">
-          { IconPlus }
-        </div>
-      </button>
-      <button className="rvt-m-card rvt-m-card--empty">
-        <div className="rvt-m-card__content">
-          { IconPlus }
-        </div>
-      </button>
+    return <div className="rvt-m-tabs__panel" tabIndex="0" role="tabpanel" id="tab-3" aria-labelledby="t-three" hidden>
+      <Card details    = { "Bus Status" }/>
+      <Card details    = { "Locations" }/>
+      <Card details    = { "Crimson Card" }/>
+      <Card details    = { "Find Food" }/>
+      <Card details    = { "Parking" }/>
+      <Card details    = { "Labs & Printers" }/>
     </div>;
   }
 }
