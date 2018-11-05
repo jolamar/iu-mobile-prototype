a701a90fc86d901f3fc0b7adcf284661aa18b952
Josh LaMar <jolamar@iu.edu>
2018-11-05 14:07:48 -0700

    center subheader title a bit

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -4,7 +4,7 @@ export class SubHeader extends Component {
   render() {
     return <div className="rvt-m-header rvt-m-subheader">
       <a href="/campus" className={"rvt-button rvt-button--plain"}>Back</a>
-      <div className="rvt-m-header__title rvt-m-subheader__title rvt-m-left-xxl rvt-p-left-lg">
+      <div className="rvt-m-header__title rvt-m-subheader__title">
         <span className="rvt-ts-16">
             <span className="rvt-text-bold">{this.props.title}</span>
           </span>
