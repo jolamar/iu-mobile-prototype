13e33d688db87ff9317a4eef505891165962d63f
Josh LaMar <jolamar@iu.edu>
2018-11-06 09:34:25 -0700

    new footer look

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -5,14 +5,28 @@ import { IconMagnifyingGlass } from '../icons'
 export class Toolbar extends Component {
 
   render() {
+
+    // Time
+    const time = new Date();
+    const hour = time.getHours()
+    const morning = hour >= 4 && hour < 12
+    const afternoon = hour >= 12 && hour < 17
+    const evening = hour >= 17 || hour < 4
+
+    // Name
+    const firstName = 'Dwight'
+
     return <div className="rvt-m-toolbar">
-      <button
-        onClick={() => alert('hey, you clicked!')}
-        className="rvt-m-search-button"
-      >
-        <span className="rvt-sr-only">Search</span>
-        { IconMagnifyingGlass }
-      </button>
+      <div className="rvt-text-center rvt-m-toolbar__content">
+        <button
+          onClick={() => alert('hey, you clicked!')}
+          className="rvt-m-search-button"
+        >
+          <span className="rvt-sr-only">Search</span>
+          { IconMagnifyingGlass }
+        </button>
+        <p>Good { morning && 'morning' }{ afternoon && 'afternoon' }{ evening && 'evening' }, {firstName}</p>
+      </div>
     </div>;
   }
 }
