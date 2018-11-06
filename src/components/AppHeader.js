18c28be5acbde2418ee369c1a88a020a46e2bc49
Josh LaMar <jolamar@iu.edu>
2018-11-06 13:25:35 -0700

    marketing lockup

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -5,10 +5,7 @@ export class AppHeader extends Component {
   render() {
     return <div className="rvt-m-header">
         <div className="rvt-m-header__title">
-          { IconTrident }
-          <span className="rvt-ts-16">
-            IU <span className="rvt-text-bold">{this.props.campus}</span>
-          </span>
+          <img src="/iu.png" className="rvt-m-header__logo" alt="IU Bloomington" />
         </div>
         {this.props.children}
       </div>;
