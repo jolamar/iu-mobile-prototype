ce53aec3dd27b81428db141442ab1546f584d934
Josh LaMar <jolamar@iu.edu>
2018-11-08 12:30:12 -0700

    parking signs

--- src/pages/Parking.js
+++ src/pages/Parking.js
@@ -8,25 +8,68 @@ export class Parking extends Component {
 
     return <div className="rvt-m-tabs__panel rvt-p-bottom-xxl" tabIndex="0" role="tabpanel" id="tab-3" aria-labelledby="t-three">
 
-      <Card className="rvt-m-top-lg"
-            title = { "Hodge Hall 1047" }
-            details = {
-              <div><span className="card__highlight--green rvt-text-bold">7 seats available</span></div>
-            }
-            links = {[
-              { title: 'Details', url: '#' },
-            ]}
-      />
+      <h2 className="rvt-ts-23 rvt-m-top-lg">Permitted parking spaces</h2>
 
-      <Card className="rvt-m-top-sm"
-            title = { "Hodge Hall 4055" }
-            details = {
-              <div><span className="card__highlight--red rvt-text-bold">Full</span></div>
-            }
-            links = {[
-              { title: 'Details', url: '#' },
-            ]}
-      />
+      <div className="rvt-grid">
+        <div className="rvt-grid__item-8">
+          <Card className="rvt-m-top-sm"
+                title = { "McNutt Parking Lot" }
+                details = "Permitted 24 hours"
+                links = {[
+                  { title: 'Getting there', url: '#' },
+                ]}
+          />
+        </div>
+        <div className="rvt-grid__item-4">
+          <div className="parking-sign">
+            <div className="parking-sign__permit">CH6</div>
+            <div className="parking-sign__allowed">Permits</div>
+            <div className="parking-sign__schedule">24 Hours</div>
+            <div className="parking-sign__consequence">Tow Zone</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="rvt-grid">
+        <div className="rvt-grid__item-8">
+          <Card className="rvt-m-top-lg"
+                title = { "White Lot" }
+                details = "Permitted until 11:59 PM"
+                links = {[
+                  { title: 'Getting there', url: '#' },
+                ]}
+          />
+        </div>
+        <div className="rvt-grid__item-4">
+          <div className="parking-sign rvt-m-top-lg">
+            <div className="parking-sign__permit">ST</div>
+            <div className="parking-sign__allowed">Permits</div>
+            <div className="parking-sign__consequence">Tow Zone</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="rvt-grid">
+        <div className="rvt-grid__item-8">
+          <Card className="rvt-m-top-lg"
+                title = { "Fee Lane Garage" }
+                details = "Permitted until 7:00 AM"
+                links = {[
+                  { title: 'Getting there', url: '#' },
+                ]}
+          />
+        </div>
+        <div className="rvt-grid__item-4">
+          <div className="parking-sign parking-sign--no-parking rvt-m-top-lg">
+            <div className="parking-sign__permit">No student parking</div>
+            <div className="parking-sign__schedule">7AM-5PM</div>
+          </div>
+        </div>
+      </div>
+
+
+
+
 
     </div>;
   }
