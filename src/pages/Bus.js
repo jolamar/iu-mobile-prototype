c00f445695c5aa9d978616b5f7dfe99fe06e526b
Josh LaMar <jolamar@iu.edu>
2018-11-13 10:10:50 -0700

    adds alert

--- src/pages/Bus.js
+++ src/pages/Bus.js
@@ -40,10 +40,25 @@ export class Bus extends Component {
 
     return <div className="rvt-m-tabs__panel rvt-p-bottom-xxl" tabIndex="0" role="tabpanel" id="tab-3" aria-labelledby="t-three">
 
-      <div className="rvt-alert rvt-alert--warning rvt-m-bottom-md" role="alertdialog" aria-labelledby="success-alert-title">
-          <h1 className="rvt-alert__title" id="success-alert-title">Bus service suspended tomorrow morning</h1>
-          <p className="rvt-alert__message">Tomorrow is the Jill Behrman Color the Campus 5k. During this event we will not be running any bus service. We expect to start service around 1:30 tomorrow. We apologize for the inconvenience.</p>
-      </div>
+
+      <button className="rvt-m-top-md rvt-m-bottom-xl rvt-alert rvt-alert--button rvt-display-flex rvt-alert--warning rvt-m-bottom-md" role="alertdialog" aria-labelledby="warning-alert-title">
+
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+            <g fill="currentColor">
+              <path d="M8,16a8,8,0,1,1,8-8A8,8,0,0,1,8,16ZM8,2a6,6,0,1,0,6,6A6,6,0,0,0,8,2Z"/>
+              <path d="M8,9A1,1,0,0,1,7,8V5A1,1,0,0,1,9,5V8A1,1,0,0,1,8,9Z"/>
+              <circle cx="8" cy="11" r="1"/>
+            </g>
+          </svg>
+        </div>
+        <h1 className="rvt-alert__title" id="warning-alert-title">Color the Campus 5k tomorrow. Expect delays</h1>
+        <div>
+          <svg className="rvt-grid__item--last" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+            <path fill="currentColor" d="M5.5,15a1,1,0,0,1-.77-1.64L9.2,8,4.73,2.64A1,1,0,0,1,6.27,1.36L11.13,7.2a1.25,1.25,0,0,1,0,1.61L6.27,14.64A1,1,0,0,1,5.5,15ZM9.6,8.48h0Zm0-1h0Z"/>
+          </svg>
+        </div>
+      </button>
 
       { !!routes && routes.map(route =>
         <Card className="rvt-m-top-sm" key={route.id} title = {
