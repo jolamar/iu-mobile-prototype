759c7adcfb2ddb6d967bd4b1cbdc569399c3fe6b
Josh LaMar <jolamar@iu.edu>
2018-11-27 11:52:40 -0700

    Updates

--- precache-manifest.c198f3752cc75faedefc4d71ac82dc63.js
+++ precache-manifest.c198f3752cc75faedefc4d71ac82dc63.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "0c20f3bed87ba90358cc",
-    "url": "/static/js/main.0c20f3be.chunk.js"
+    "revision": "2ea323e13b97e4975286",
+    "url": "/static/js/main.2ea323e1.chunk.js"
   },
   {
     "revision": "d45503091b5aad5ad592",
     "url": "/static/js/1.d4550309.chunk.js"
   },
   {
-    "revision": "0c20f3bed87ba90358cc",
+    "revision": "2ea323e13b97e4975286",
     "url": "/static/css/main.25f31ea1.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.b32de0f1.chunk.css"
   },
   {
-    "revision": "8c0294ec2158775db0bd8fddf6b0141b",
+    "revision": "033693deffbaf746e31fe1b292c1affb",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a15b6d13c72bb256bd48d5713a61d6b3.js"
+  "/precache-manifest.c198f3752cc75faedefc4d71ac82dc63.js"
 );
 
 workbox.clientsClaim();
