294babba0162789c90c788d2781583fb134edcd1
Josh LaMar <jolamar@iu.edu>
2018-11-28 09:54:42 -0700

    settings

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
+import 'rivet-switch/dist/css/rivet-switch.min.css';
+const Switch = require('rivet-switch/dist/js/rivet-switch.min.js');
+let idx = 0;
+const uuid = () => idx++;
 
 export class Settings extends Component {
 
+  componentDidMount() {
+    Switch.init()
+  }
+
   render() {
     return <div className="rvt-m-tabs__panel rvt-p-bottom-xxl" tabIndex="0" role="tabpanel" id="tab-5" aria-labelledby="t-five">
-      <div className="rvt-m-card">
-        <div className="rvt-m-card__content">
-          <div className="rvt-m-bottom-sm">Settings</div>
+      <p className="rvt-ts-23 rvt-m-top-remove rvt-m-bottom-md">App settings</p>
+
+      <div className="cards">
+        <div className="card">
+          <div className="rvt-display-flex rvt-p-all-md">
+            <div>Left-handed search</div>
+            <div className="rvt-grid__item--last">
+              <button className="rvt-switch" data-switch="email-switch" role="switch" aria-checked="false">
+                <span className="rvt-switch__on">On</span>
+                <span className="rvt-switch__off">Off</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="rvt-display-flex rvt-p-all-md">
+            <div>Parking permit</div>
+            <div className="rvt-grid__item--last">
+              <select>
+                <option>ST</option>
+                <option>STFA</option>
+                <option>STSP</option>
+                <option>Evening</option>
+                <option>EvenigFA</option>
+                <option>EveninSP</option>
+                <option>GSU</option>
+                <option>EM-P</option>
+                <option>EM-S</option>
+              </select>
+            </div>
+          </div>
         </div>
       </div>
     </div>;
