4ee1392755e2d8021aafd027aaf2d7eb0e6c9e00
Josh LaMar <jolamar@iu.edu>
2018-11-28 09:14:51 -0700

    Updates

---
+++ precache-manifest.0023a5196b8eaf27461b781f2336b1be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b4d1172022a578e6c92",
+    "url": "/static/js/main.5b4d1172.chunk.js"
+  },
+  {
+    "revision": "1883379799d193020f37",
+    "url": "/static/js/1.18833797.chunk.js"
+  },
+  {
+    "revision": "5b4d1172022a578e6c92",
+    "url": "/static/css/main.04983180.chunk.css"
+  },
+  {
+    "revision": "1883379799d193020f37",
+    "url": "/static/css/1.dfc44139.chunk.css"
+  },
+  {
+    "revision": "af2813c488e98caf2f666b710516682f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f867b185b5aea593fff8edaf10b06f3.js"
+  "/precache-manifest.0023a5196b8eaf27461b781f2336b1be.js"
 );
 
 workbox.clientsClaim();
