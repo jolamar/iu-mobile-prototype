b9945ef8847224ebd19c1873a5461186a3cfe5e0
Josh LaMar <jolamar@iu.edu>
2018-11-29 14:03:52 -0700

    new avatar

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Avatar = props => {
   return (
-    <div className="rvt-m-avatar rvt-m-avatar--decorated rvt-m-left-auto">
+    <div className="rvt-m-avatar rvt-m-left-auto">
       <img src={props.url} alt={props.alt} />
     </div>
   )
